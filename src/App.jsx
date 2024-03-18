a471ac75358fcb22f35c606692b84cc86d9cba93
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-18 22:27:37 +0530

    Features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 
 import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <main className="bg-black">
-      <Navbar /> <Hero /> <Highlights /> <Model />
+      <Navbar /> <Hero /> <Highlights /> <Model /> <Features />
     </main>
   );
 };
